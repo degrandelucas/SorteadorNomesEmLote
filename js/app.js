4641d667bda090ef870ff6af17d9599f114083aa
degrandelucas <lucasdegrande15@gmail.com>
2025-02-03 21:06:40 -0300

    funcoes adicionar() e reiniciar() implantadas

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
 let listaAmigos = [];
 
-function adicionar(){
+function adicionar() {
+  let amigo = document.getElementById("nome-amigo").value;
 
+  if (amigo === "") {
+    alert("Digite o nome do amigo!");
+    return;
+  }
+
+  if (listaAmigos.includes(amigo)) {
+    alert("Nome do amigo já está na lista!");
+    return;
+  }
+
+  listaAmigos.push(amigo);
+
+  document.getElementById("lista-amigos").innerHTML = listaAmigos;
+  document.getElementById("nome-amigo").value = "";
 }
 
-function sortear(){
-    
+function sortear() {
+    if (listaAmigos.length < 4) {
+        alert("Adicione mais amigos para sortear!");
+        return;
+    } else {
+
+        let sorteio = document.getElementById("lista-sorteio");
+        sorteio.innerHTML = '';
+    }
 }
 
-function reiniciar(){
-
+function reiniciar() {
+    document.getElementById("lista-amigos").innerHTML = '';
+    document.getElementById("lista-sorteio").innerHTML = '';
 }
-
