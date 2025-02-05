5f28b0dfe63ecb820539a05775ddedf981f1a328
degrandelucas <lucasdegrande15@gmail.com>
2025-02-05 12:02:20 -0300

    mudanca na funcao sortear(), sendo possivel sortear varias vezes a mesma lista, sem repetir

--- js/app.js
+++ js/app.js
@@ -1,55 +1,56 @@
-  let listaNomes = [];
+    let listaNomes = [];
 
-  function adicionar() {
-    let nome = document.getElementById("nome").value.trim().toUpperCase();
+    function adicionar() {
+      let nome = document.getElementById("nome").value.trim().toUpperCase();
 
-    if (nome === "") {
-      alert("Digite um nome!");
-      return;
-    }
+      if (nome === "") {
+        alert("Digite um nome!");
+        return;
+      }
 
-    if (listaNomes.includes(nome)) {
-      alert("Nome já está na lista!");
+      if (listaNomes.includes(nome)) {
+        alert("Nome já está na lista!");
+        document.getElementById("nome").value = "";
+        return;
+      }
+
+      listaNomes.push(nome);
+
+      document.getElementById("lista-nomes").innerHTML = listaNomes;
       document.getElementById("nome").value = "";
-      return;
     }
 
-    listaNomes.push(nome);
+    function sortear() {
+      let sorteio = document.getElementById("lista-sorteio");
+      sorteio.innerHTML = '';
 
-    document.getElementById("lista-nomes").innerHTML = listaNomes;
-    document.getElementById("nome").value = "";
-  }
-
-  function sortear() {
-    let sorteio = document.getElementById("lista-sorteio");
-
-    if (listaNomes.length < 2) {
-      alert("Adicione mais amigos para sortear!");
-      return;
-    } else {
-      embaralha(listaNomes);
-    }
-    for (let i = 0; i < listaNomes.length; i++) {
-      if (i < listaNomes.length) {
-        sorteio.innerHTML +=
-          listaNomes[i] + "<br>";
+      if (listaNomes.length < 2) {
+        alert("Adicione mais amigos para sortear!");
+        return;
+      } else {
+        embaralha(listaNomes);
+      }
+      for (let i = 0; i < listaNomes.length; i++) {
+        if (i < listaNomes.length) {
+          sorteio.innerHTML +=
+            listaNomes[i] + "<br>";
+        }
       }
     }
-  }
 
-  function embaralha(lista) {
-    for (let indice = lista.length; indice; indice--) {
-      let indiceAleatorio = Math.floor(Math.random() * indice);
-      [lista[indice - 1], lista[indiceAleatorio]] = [
-        lista[indiceAleatorio],
-        lista[indice - 1],
-      ];
+    function embaralha(lista) {
+      for (let indice = lista.length; indice; indice--) {
+        let indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [
+          lista[indiceAleatorio],
+          lista[indice - 1],
+        ];
+      }
+      return lista;
     }
-    return lista;
-  }
 
-  function reiniciar() {
-    document.getElementById("lista-nomes").innerHTML = "";
-    document.getElementById("lista-sorteio").innerHTML = "";
-    listaNomes = [];
-  }
+    function reiniciar() {
+      document.getElementById("lista-nomes").innerHTML = "";
+      document.getElementById("lista-sorteio").innerHTML = "";
+      listaNomes = [];
+    }
